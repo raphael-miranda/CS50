992b0bbdf74730aebeaa103bc1826b49d382b3b8
Raphael Miranda <108379868+raphael-miranda@users.noreply.github.com>
2024-05-29 17:20:26 -0300

    Create mario.c

---
+++ pset1/mario/hard/mario.c
@@ -0,0 +1,42 @@
+// en-us
+#include <cs50.h>
+#include <stdio.h>
+
+void generatePiramyd();
+
+int main()
+{
+    generatePiramyd();
+}
+
+void generatePiramyd() {
+    int height;
+
+    do
+    {
+        height = get_int("Width: "); // Get height by user input
+    }
+    while (height < 1 || height > 8);
+
+    for (int i = 0; i < height; i++) { // Set Colums size by height
+
+        // Draw whitespaces before hashes
+        for (int j = (i+1); j < height; j++) {
+            printf(" ");
+        }
+
+        // Draw Hashes
+        for (int j = 0; j <= i; j++) {
+            printf("#");
+        }
+
+        printf("  "); // Draw whitespaces between the colums
+
+        // Draw right hashes
+        for (int k = 0; k <= i; k++) {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
